c16b5225bf29a1e986852e70f2ed9bfb0e7068da
Emily Melendez <emelendez@chariotsolutions.com>
2019-01-26 13:40:08 -0500

    css start for pokedex

--- frontend/src/Pokedex/pokedex.js
+++ frontend/src/Pokedex/pokedex.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import styles from "./pokedex.module.scss";
+import cx from 'classnames';
 
 class Pokedex extends Component {
     constructor(props){
@@ -8,7 +10,24 @@ class Pokedex extends Component {
     render() {
         return(
             <div>
-                <p>Pokedex</p>
+                <div className={styles.pokedex}>
+                    <div className={styles.pokedexMain}>
+                        <div className={styles.pokedexHeader}>
+                            <div className={styles.circle}>
+
+                            </div>
+                        </div>
+                        <div className={styles.pokedexScreen}>
+
+                        </div>
+
+                    </div>
+                    <div className={styles.pokedexDoor}>
+                        <div className={styles.doorInset}>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
