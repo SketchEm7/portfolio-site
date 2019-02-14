aa5601accb390e5c691d0df862f01b065e491555
Emily Melendez <emelendez@chariotsolutions.com>
2019-02-14 08:58:18 -0500

    added title to todraw list

--- frontend/src/ToDrawApp/toDrawApp.js
+++ frontend/src/ToDrawApp/toDrawApp.js
@@ -7,20 +7,28 @@ class ToDrawApp extends Component {
     constructor(props) {
         super(props);
         this.state={
+            title: '',
             term: '',
             items: [],
         }
     }
 
-    onChange = (event)=> {
-        this.setState({term:event.target.value});
+    onChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
     };
 
     onSubmit = (event)=> {
         event.preventDefault();
         this.setState({
             term: '',
-            items: [...this.state.items, this.state.term],
+            title: '',
+            items: [...this.state.items,
+                {
+                    term: this.state.term,
+                    title: this.state.title
+                }],
         });
     };
 
@@ -32,9 +40,12 @@ class ToDrawApp extends Component {
                <ul>
                    {
                        props.items.map((item, index) =>
-                           <React.Fragment>
+                           <React.Fragment key={index} >
                                <hr/>
-                               <li key={index} className={styles.item}>{item}</li>
+                               <li className={styles.item}>
+                                   <h3 className={styles.title}>{item.title.toUpperCase()}</h3>
+                                   <p className={styles.description}>{item.term}</p>
+                               </li>
                            </React.Fragment>)
                    }
                </ul>
@@ -44,7 +55,13 @@ class ToDrawApp extends Component {
         return(
             <div className={styles.list}>
                 <form onSubmit={this.onSubmit}>
-                    <input value={this.state.term} onChange={this.onChange}/>
+                    <span>Title</span>
+                    <div>
+                        <input className={styles.titleInput} aria-label={"title"} value={this.state.title} name="title" onChange={this.onChange}/>
+                    </div>
+
+                    <span>Description</span>
+                    <input aria-label={"term"} value={this.state.term} name="term" onChange={this.onChange}/>
                     <div>Palette here:
                         <ChromePicker/>
                     </div>
