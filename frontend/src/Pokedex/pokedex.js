05e581cdd8ae642b9393b2a7708cc9b6cfd1b2d4
Emily Melendez <emelendez@chariotsolutions.com>
2019-07-23 22:56:08 -0400

    pokedex updates

--- frontend/src/Pokedex/pokedex.js
+++ frontend/src/Pokedex/pokedex.js
@@ -2,21 +2,26 @@ import React, {Component} from "react";
 import styles from "./pokedex.module.scss";
 import axios from "axios";
 import Autosuggest from 'react-autosuggest';
+// import pokemonImage from `../assets/imgs/pokemon/${pokemonId}.png`;
+
 
 
 class Pokedex extends Component {
 
     pokedex;
+    pokemon;
     getSuggestions;
     getSuggestionValue;
     renderSuggestion;
 
     constructor(props){
-        super(props)
+        super(props);
         this.state = {
-            pokemon: null,
+            pokemonName: null,
             value: '',
             suggestions: [],
+            typeOneBackgroundColor: '',
+            typeTwoBackgroundColor: '',
         }
 
         this.doShit();
@@ -26,7 +31,6 @@ class Pokedex extends Component {
     doShit() {
         // Teach Autosuggest how to calculate suggestions for any given input value.
         this.getSuggestions = value => {
-            console.log(value);
             const inputValue = value.trim().toLowerCase();
             const inputLength = inputValue.length;
 
@@ -35,12 +39,12 @@ class Pokedex extends Component {
             );
         };
 
-// When suggestion is clicked, Autosuggest needs to populate the input
-// based on the clicked suggestion. Teach Autosuggest how to calculate the
-// input value for every given suggestion.
+        // When suggestion is clicked, Autosuggest needs to populate the input
+        // based on the clicked suggestion. Teach Autosuggest how to calculate the
+        // input value for every given suggestion.
         this.getSuggestionValue = suggestion => suggestion.name;
 
-// Use your imagination to render suggestions.
+        // Use your imagination to render suggestions.
         this.renderSuggestion = suggestion => (
             <div>
                 {suggestion.name}
@@ -51,7 +55,7 @@ class Pokedex extends Component {
     async componentDidMount(){
         this.pokedex = await axios.get(`/pokedex`);
         this.setState({
-            pokemon: this.pokedex.data,
+            pokedex: this.pokedex.data,
         });
         this.doShit();
     }
@@ -78,6 +82,169 @@ class Pokedex extends Component {
         });
     };
 
+    onSuggestionSelected = async (selected, {suggestionValue}) => {
+        this.pokemon = await axios.get(`/pokedex/${suggestionValue}`);
+
+        this.setState({
+            pokemonName: this.pokemon.data.name,
+            pokemonId: this.pokemon.data.id,
+            typeOne: this.pokemon.data.type_one,
+            typeTwo: this.pokemon.data.type_two,
+            hp: this.pokemon.data.hp,
+            attack: this.pokemon.data.attack,
+            defense: this.pokemon.data.defense,
+            specialAttack: this.pokemon.data.sp_attack,
+            specialDefense: this.pokemon.data.sp_defense,
+            speed: this.pokemon.data.speed,
+        });
+
+        switch (this.state.typeOne) {
+            case 'Fire':
+                this.setState({
+                    typeOneBackgroundColor: '#d00606'
+                });
+                break;
+            case 'Grass':
+                this.setState({
+                    typeOneBackgroundColor: '#028602'
+                });
+                break;
+            case 'Water':
+                this.setState({
+                    typeOneBackgroundColor: '#022882'
+                });
+                break;
+            case 'Bug':
+                this.setState({
+                    typeOneBackgroundColor: '#999900'
+                });
+                break;
+            case 'Poison':
+                this.setState({
+                    typeOneBackgroundColor: '#990099'
+                });
+                break;
+            case 'Flying':
+                this.setState({
+                    typeOneBackgroundColor: '#009999'
+                });
+                break;
+            case 'Normal':
+                this.setState({
+                    typeOneBackgroundColor: '#444400'
+                });
+                break;
+            case 'Electric':
+                this.setState({
+                    typeOneBackgroundColor: '#ce9e03'
+                });
+                break;
+            case 'Rock':
+                this.setState({
+                    typeOneBackgroundColor: '#4b433e'
+                });
+                break;
+            case 'Ground':
+                this.setState({
+                    typeOneBackgroundColor: '#906901'
+                });
+                break;
+            case 'Psychic':
+                this.setState({
+                    typeOneBackgroundColor: '#6600a3'
+                });
+                break;
+            case 'Fighting':
+                this.setState({
+                    typeOneBackgroundColor: '#990100'
+                });
+                break;
+            case 'Dragon':
+                this.setState({
+                    typeOneBackgroundColor: '#275074'
+                });
+                break;
+            case 'Fairy':
+                this.setState({
+                    typeOneBackgroundColor: '#d47d86'
+                });
+                break;
+        }
+
+        switch (this.state.typeTwo) {
+            case 'Fire':
+                this.setState({
+                    typeTwoBackgroundColor: '#d00606'
+                });
+                break;
+            case 'Grass':
+                this.setState({
+                    typeTwoBackgroundColor: '#028602'
+                });
+                break;
+            case 'Water':
+                this.setState({
+                    typeTwoBackgroundColor: '#022882'
+                });
+                break;
+            case 'Bug':
+                this.setState({
+                    typeTwoBackgroundColor: '#999900'
+                });
+                break;
+            case 'Poison':
+                this.setState({
+                    typeTwoBackgroundColor: '#990099'
+                });
+                break;
+            case 'Flying':
+                this.setState({
+                    typeTwoBackgroundColor: '#009999'
+                });
+                break;
+            case 'Normal':
+                this.setState({
+                    typeTwoBackgroundColor: '#444400'
+                });
+                break;
+            case 'Electric':
+                this.setState({
+                    typeTwoBackgroundColor: '#ce9e03'
+                });
+                break;
+            case 'Rock':
+                this.setState({
+                    typeTwoBackgroundColor: '#4b433e'
+                });
+                break;
+            case 'Ground':
+                this.setState({
+                    typeTwoBackgroundColor: '#906901'
+                });
+                break;
+            case 'Psychic':
+                this.setState({
+                    typeTwoBackgroundColor: '#6600a3'
+                });
+                break;
+            case 'Fighting':
+                this.setState({
+                    typeTwoBackgroundColor: '#990100'
+                });
+                break;
+            case 'Dragon':
+                this.setState({
+                    typeTwoBackgroundColor: '#275074'
+                });
+                break;
+            default:
+                this.setState({
+                    typeTwoBackgroundColor: 'transparent'
+                });
+        }
+
+    };
+
     render() {
         const { value, suggestions } = this.state;
 
@@ -88,6 +255,8 @@ class Pokedex extends Component {
             onChange: this.onChange
         };
 
+        const pokemonImage = <img className={styles.pokemonImage} src={`${process.env.PUBLIC_URL}/pokemon/${this.state.pokemonId}.png`} alt={"pokemon-img"}/>
+        const showImage = this.state.pokemonId ? pokemonImage : null;
         return(
             <div>
                 <div className={styles.pokedex}>
@@ -102,16 +271,19 @@ class Pokedex extends Component {
                                 onSuggestionsClearRequested={this.onSuggestionsClearRequested}
                                 getSuggestionValue={this.getSuggestionValue}
                                 renderSuggestion={this.renderSuggestion}
+                                onSuggestionSelected={this.onSuggestionSelected}
                                 inputProps={inputProps}
                                 theme={styles}
                             />
                         </div>
 
                         <div className={styles.pokedexScreen}>
-
+                            <h1 className={styles.pokemonName}>{!!this.pokemon ? this.state.pokemonName : '' }</h1>
+                            {showImage}
+                            <div className={styles.pokemonId}>#{this.state.pokemonId}</div>
+                            <span style={{backgroundColor: this.state.typeOneBackgroundColor}} className={styles.type}>{this.state.typeOne}</span>
+                            <span style={{backgroundColor: this.state.typeTwoBackgroundColor}} className={styles.type}>{this.state.typeTwo}</span>
                         </div>
-
-
 
                     </div>
                 </div>
